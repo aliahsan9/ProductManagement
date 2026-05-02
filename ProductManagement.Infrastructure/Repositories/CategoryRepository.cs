07a3eb60d601716bf39ab202352df630719857d9
Ali Ahsan <aliahsan8751@gmail.com>
2026-05-02 16:04:32 +0500

    updated dto

--- ProductManagement.Infrastructure/Repositories/CategoryRepository.cs
+++ ProductManagement.Infrastructure/Repositories/CategoryRepository.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductManagement.Application.Interfaces.Repositories;
+using ProductManagement.Domain.Entities;
+using ProductManagement.Infrastructure.Persistence;
 
 namespace ProductManagement.Infrastructure.Repositories
 {
-    internal class CategoryRepository
+    public class CategoryRepository : BaseRepository<CategoryRepository> ICategoryRepository
     {
+        public CategoryRepository(AppDbContext context) : base(context) { }
+
+        public async Task<Category?> GetByIdAsync(int id)
+        {
+            return await _context.Categories
+                .Include(c => c.Products)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
     }
 }
--- ProductManagement.Infrastructure/Repositories/ProductRepository.cs
+++ ProductManagement.Infrastructure/Repositories/ProductRepository.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using ProductManagement.Application.Interfaces.Repositories;
+using ProductManagement.Domain.Entities;
+using ProductManagement.Infrastructure.Persistence;
 
 namespace ProductManagement.Infrastructure.Repositories
 {
-    internal class ProductRepository
+    public class ProductRepository : BaseRepository<ProductRepository>, IProductRepository
     {
+        public ProductRepository(AppDbContext context) : base (context) { }
+
+        public async Task<List<Product>> GetAllAsync()
+        {
+            return await _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.ProductImages)
+                .ToListAsync();
+        }
+        public async Task<Product?> GetByIdAsync(int id)
+        {
+            return await _context.Products
+                .Include(P => P.Category)
+                .Include(P => P.ProductImages)
+                .FirstOrDefaultAsync(P => P.Id == id);
+
+        }
+        public async Task<Product> AddAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+        }
+        public async Task UpdateAsync(Product product)
+        {
+            _context.Products.Update(product);
+        }
+        public async Task DeleteAsync(Product product)
+        {
+            _context.Products.Remove(product);
+        }
     }
 }
