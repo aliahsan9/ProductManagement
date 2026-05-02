e48cb0c0a3dadbcb0b1e0263905af8511bf47fb1
Ali Ahsan <aliahsan8751@gmail.com>
2026-05-02 15:06:15 +0500

    added product controller

---
+++ ProductManagement.API/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using ProductManagement.Application.Services;
+using ProductManagement.Domain.Entities;
+
+namespace ProductManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService _service;
+        public ProductController(ProductService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _service.GetAllAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var products = await _service.GetByIdAsync(id);
+            if (products == null)
+                return NotFound();
+            return Ok(products);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductResponseDto dto)
+        {
+            var productId = await _service.CreateAsync(dto);
+
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id = productId },
+               productId
+               );
+
+        }
+    }
+}
