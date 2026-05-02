50eb39810f2e96a91dfadc8ab9a39d9b139b13db
Ali Ahsan <aliahsan8751@gmail.com>
2026-05-02 15:10:24 +0500

    updated dto

--- ProductManagement.API/Program.cs
+++ ProductManagement.API/Program.cs
@@ -5,7 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = false;
+    });
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
