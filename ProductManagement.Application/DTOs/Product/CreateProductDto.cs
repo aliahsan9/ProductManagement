8ae043e96e0f51efb9f8364f13c8d68ff4e8a5b9
Ali Ahsan <aliahsan8751@gmail.com>
2026-05-02 15:09:30 +0500

    updated dto

--- ProductManagement.Application/DTOs/Product/CreateProductDto.cs
+++ ProductManagement.Application/DTOs/Product/CreateProductDto.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProductManagement.Application.DTOs.Product
 {
     public class CreateProductDto
     {
+        [Required]
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        [Range(0.1, double.MaxValue)]
         public decimal Price { get; set; }
+        [Required]
         public string SKU { get; set; } = string.Empty;
+        [Range(0, int.MaxValue)]
         public int StockQuantity { get; set; }
         public int CategroyId { get; set; }
 
