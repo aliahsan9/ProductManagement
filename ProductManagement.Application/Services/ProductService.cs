9be0cdf80253890e0185dfe50be890db350b513a
Ali Ahsan <aliahsan8751@gmail.com>
2026-05-02 17:29:42 +0500

    updated dto

--- ProductManagement.Application/Services/ProductService.cs
+++ ProductManagement.Application/Services/ProductService.cs
@@ -1,35 +1,45 @@
 using AutoMapper;
 using ProductManagement.Application.DTOs.Product;
-using ProductManagement.Application.Interfaces.Repositories;
 using ProductManagement.Application.Interfaces.Services;
+using ProductManagement.Application.Interfaces.UnitOfWork;
 using ProductManagement.Domain.Entities;
 
-namespace ProductManagement.Application.Services
+namespace ProductManagement.Application.Services;
+
+public class ProductService : IProductService
 {
-    public class ProductService(IProductRepository repository, Mapper mapper) : IProductService
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public ProductService(IUnitOfWork unitOfWork, IMapper mapper)
     {
-        private readonly IProductRepository _repository = repository;
-        private readonly Mapper _mapper = mapper;
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
 
-        public async Task<List<ProductResponseDto>> GetAllAsync()
-        {
-            var products = await _repository.GetAllAsync();
-            return _mapper.Map<List<ProductResponseDto>>(products);
-        }
-        public async Task<ProductResponseDto?> GetByIdAsync(Guid id)
-        {
-            var products = await _repository.GetByIdAsync(id);
-            if (products == null) return null;
+    public async Task<List<ProductResponseDto>> GetAllAsync()
+    {
+        var products = await _unitOfWork.Products.GetAllAsync();
+        return _mapper.Map<List<ProductResponseDto>>(products);
+    }
 
-            return _mapper.Map<ProductResponseDto>(products);
-        }
-        public async Task<int> CreateAsync(CreateProductDto dto)
-        {
-            var product _mapper.Map<Product>(dto);
+    public async Task<ProductResponseDto?> GetByIdAsync(int id)
+    {
+        var product = await _unitOfWork.Products.GetByIdAsync(id);
 
-            await _repository.CreateAsync(product);
-            return product.Id;
+        if (product == null)
+            return null;
 
-        }
+        return _mapper.Map<ProductResponseDto>(product);
+    }
+
+    public async Task<int> CreateAsync(CreateProductDto dto)
+    {
+        var product = _mapper.Map<Product>(dto);
+
+        await _unitOfWork.Products.AddAsync(product);
+        await _unitOfWork.SaveChangesAsync(); // 🔥 SINGLE COMMIT POINT
+
+        return product.Id;
     }
 }
